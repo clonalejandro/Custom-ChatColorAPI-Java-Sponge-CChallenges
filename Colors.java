ea63a8d6427d3072a519ee3f39ddca51e5d383f1
clonalejandro <contact@clonalejandro.me>
2017-04-19 23:52:39 +0200

    Create Colors.java

---
+++ Colors.java
@@ -0,0 +1,46 @@
+package me.clonalejandro.Minemcore.utils.api.colors;
+
+import org.spongepowered.api.text.format.TextColor;
+
+/**
+ * Created by alejandrorioscalera on 18/4/17.
+ */
+public interface Colors {
+
+
+    /** METHODS **/
+
+    public TextColor BLACK();
+
+    public TextColor DARK_BLUE();
+
+    public TextColor DARK_GREEN();
+
+    public TextColor DARK_AQUA();
+
+    public TextColor DARK_RED();
+
+    public TextColor DARK_PURPLE();
+
+    public TextColor GOLD();
+
+    public TextColor GRAY();
+
+    public TextColor DARK_GRAY();
+
+    public TextColor BLUE();
+
+    public TextColor GREEN();
+
+    public TextColor AQUA();
+
+    public TextColor RED();
+
+    public TextColor LIGHT_PURPLE();
+
+    public TextColor YELLOW();
+
+    public TextColor WHITE();
+
+
+}
